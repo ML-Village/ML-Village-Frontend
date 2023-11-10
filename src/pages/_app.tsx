bb8643338c31700dfe29e3e1c89126916d8cd7a0
Eason Chai <echai2905@gmail.com>
2023-11-10 23:08:28 +0300

    feat: Add preline

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,23 @@
+"use client";
+
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
+import { StarknetConfig, InjectedConnector } from "@starknet-react/core";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     import("preline");
   }, []);
 
-  return <Component {...pageProps} />;
+  const connectors = [
+    new InjectedConnector({ options: { id: "braavos" } }),
+    new InjectedConnector({ options: { id: "argentX" } }),
+  ];
+
+  return (
+    <StarknetConfig connectors={connectors}>
+      <Component {...pageProps} />
+    </StarknetConfig>
+  );
 }
