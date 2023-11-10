a3ccdf70d6937ed7f4800551a065b41a758b2e16
Eason Chai <echai2905@gmail.com>
2023-11-10 18:54:48 +0300

    chore: Add styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,33 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/preline/dist/*.js",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        hero: "url('/assets/background.png')",
+        gradient: "url('/assets/gradient.png')",
+      },
+      colors: {
+        brand: {
+          offwhite: "#F7F7F7",
+          text: "#333333",
+          primary: "#0B98FF",
+          secondary: "#012677",
+          tertiary: "#2A4D70",
+          code: "#282C34",
+        },
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("preline/plugin")],
+};
+export default config;
