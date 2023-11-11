fd73b7c832a33f0be47f4b4e22f163496c0b5a66
Eason Chai <echai2905@gmail.com>
2023-11-11 10:15:49 +0300

    feat: Add toast component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
 import { StarknetConfig, InjectedConnector } from "@starknet-react/core";
+import { Toaster } from "react-hot-toast";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
@@ -18,6 +19,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <StarknetConfig connectors={connectors}>
       <Component {...pageProps} />
+      <Toaster position="top-center" reverseOrder={false} />
     </StarknetConfig>
   );
 }
