ed9a788b509162c7521e7fbb5e500cbc30cf5f05
Eason Chai <echai2905@gmail.com>
2023-11-11 12:46:44 +0300

    feat: Add charts

--- src/pages/models/[id].tsx
+++ src/pages/models/[id].tsx
@@ -4,40 +4,68 @@ import { ibm } from "@/styles/fonts";
 import clsx from "clsx";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import Chart from 'react-apexcharts'
+import { useState, useEffect } from "react";
+// import Chart from "react-apexcharts";
+import dynamic from "next/dynamic";
+
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+
+const useHasHydrated = () => {
+  const [hasHydrated, setHasHydrated] = useState<boolean>(false);
+
+  useEffect(() => {
+    setHasHydrated(true);
+  }, []);
+
+  return hasHydrated;
+};
 
 export default function ModelInfo() {
   const router = useRouter();
   const { id } = router.query;
+  const hasHydrated = useHasHydrated();
 
   const chartConfig = {
     options: {
-        chart: {
+      chart: {
         height: 350,
-        type: 'area'
+        type: "area",
       },
       dataLabels: {
-        enabled: false
+        enabled: false,
       },
       stroke: {
-        curve: 'smooth'
+        curve: "smooth",
       },
       xaxis: {
-        type: 'datetime',
-        categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+        type: "datetime",
+        categories: [
+          "2018-09-19T00:00:00.000Z",
+          "2018-09-19T01:30:00.000Z",
+          "2018-09-19T02:30:00.000Z",
+          "2018-09-19T03:30:00.000Z",
+          "2018-09-19T04:30:00.000Z",
+          "2018-09-19T05:30:00.000Z",
+          "2018-09-19T06:30:00.000Z",
+        ],
       },
       tooltip: {
         x: {
-          format: 'dd/MM/yy HH:mm'
+          format: "dd/MM/yy HH:mm",
         },
       },
     },
-    series: [{
-      name: 'series-1',
-      data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
-    }]
-  }
-
+    series: [
+      {
+        name: "Views",
+        data: [31162, 40162, 28162, 51162, 42162, 109162, 100162],
+      },
+      {
+        name: "Downloads",
+        data: [11162, 32162, 45162, 32162, 34162, 52162, 41162],
+      },
+    ],
+  };
 
   return (
     <main
@@ -47,75 +75,78 @@ export default function ModelInfo() {
       )}
     >
       <Navbar />
-        
-        <div className="container grid grid-cols-2 justify-center items-center mx-auto
+
+      <div
+        className="container grid grid-cols-2 justify-center items-center mx-auto
         px-20 pt-8
-        ">
-          <div className="
+        "
+      >
+        <div
+          className="
           rounded-lg w-auto h-auto overflow-hidden
-          "><img src="/assets/models/Model 3.png" alt="" /></div>
-          
-          
-          <div className="px-16">
-            <h1 className="font-bold text-4xl mb-3 w-full">
-            GPT-420
-            </h1>
-            <p className="my-3 text-sm">
-              Upload your ONNX file into our platform, and we will handle the
-              rest. Everything from automatic conversion, model runs, inference
-              and proof generation, we got it covered.
-            </p>
+          "
+        >
+          <img src="/assets/models/Model 3.png" alt="" />
+        </div>
 
+        <div className="px-16">
+          <h1 className="font-bold text-4xl mb-4 w-full">GPT-420</h1>
+          <p className="mb-8 text-sm">
+            Upload your ONNX file into our platform, and we will handle the
+            rest. Everything from automatic conversion, model runs, inference
+            and proof generation, we got it covered.
+          </p>
 
-            {/* metrics bar */}
-            <div className="grid grid-cols-3 mb-3">
-              <div>
-                <p className="">Model Type</p>
-                <p className="font-bold">Classifier</p>
-              </div>
-
-              <div>
-                <p className="">Training Data Size</p>
-                <p className="font-bold">256 GB</p>
-              </div>
-
-              <div>
-                <p className="">Accuracy</p>
-                <p className="font-bold">99.2%</p>
-              </div>
-
+          {/* metrics bar */}
+          <div className="grid grid-cols-3 mb-8">
+            <div>
+              <p className="mb-2">Model Type</p>
+              <p className="font-bold">Classifier</p>
             </div>
 
-            {/* Statistics Chart */}
-            <div className="my-2">
-              <h3 className="font-bold text-xl">
-              Statistics
-              </h3>
-              {/* chart itself */}
-              <Chart options={chartConfig.options} series={chartConfig.series} type="bar" width={450} height={250} />
+            <div>
+              <p className="mb-2">Training Data Size</p>
+              <p className="font-bold">256 GB</p>
             </div>
 
-              
+            <div>
+              <p className="mb-2">Accuracy</p>
+              <p className="font-bold">99.2%</p>
+            </div>
+          </div>
 
-            <div className="text-lg">Model Subscription Price</div>
-            <div className="font-bold text-lg mb-3">$420.69/mth</div>
+          {/* Statistics Chart */}
+          <div className="mb-8">
+            <h3 className="font-bold text-xl mb-4">Statistics</h3>
+            {/* chart itself */}
+            {hasHydrated ? (
+              <Chart
+                options={chartConfig.options}
+                series={chartConfig.series}
+                type="area"
+                width={450}
+                height={250}
+              />
+            ) : (
+              <></>
+            )}
+          </div>
 
-            
-            <button
-                type="button"
-                className="py-3 px-4 inline-flex items-center 
+          <div className="text-lg mb-2">Model Subscription Price</div>
+          <div className="font-bold text-lg mb-8">$420.69/mth</div>
+
+          <button
+            type="button"
+            className="py-3 px-4 inline-flex items-center 
                 gap-x-2 text-sm font-semibold 
                 rounded-lg border border-transparent 
                 bg-brand-primary text-white 
                 hover:bg-blue-600 disabled:opacity-50 disabled:pointer-events-none"
-              >
-                Purchase Model
-              </button>
-
-            
-          
-          </div>
+          >
+            Purchase Model
+          </button>
         </div>
+      </div>
     </main>
   );
 }
